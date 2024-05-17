fab999391033e10307b028800ea61f0c3dc10e6d
JaimeMarti <jaimarher@alu.edu.gva.es>
2024-05-17 17:10:53 +0200

    Inicializado repositorio

---
+++ js/main.js
@@ -0,0 +1,54 @@
+function setCredentials(email, password) {
+    setCookie("email", email, 14);
+    setCookie("password", password, 14);
+}
+
+function getEmail() {
+    return getCookie("email");
+}
+
+function getPassword() {
+    return getCookie("password");
+}
+
+function login() {
+    let email = document.getElementById("email");
+    let password = document.getElementById("contrasenya");
+    setCredentials(email.value, password.value);
+}
+
+let loginForm = document.getElementById("login-form");
+loginForm.addEventListener("submit", event => {
+    event.preventDefault();
+    login();
+});
+
+// https://stackoverflow.com/a/24103596
+function setCookie(name, value, days) {
+    let expires = "";
+    if(days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + btoa(value || "")  + expires + "; path=/";
+}
+
+// https://stackoverflow.com/a/24103596
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i=0; i < ca.length; i++) {
+        let c = ca[i];
+        while(c.charAt(0) == ' ')
+            c = c.substring(1, c.length);
+        if(c.indexOf(nameEQ) == 0)
+            return atob(c.substring(nameEQ.length, c.length));
+    }
+    return null;
+}
+
+// https://stackoverflow.com/a/24103596
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
